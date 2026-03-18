fdc22aab158120684feeb003153da1e77d86fd10
DOUGLAS ROCHA MENDES <61594551+douglasrm87@users.noreply.github.com>
2026-03-18 23:50:46 +0000

    Ajuste css login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Login.module.css';
 
 // Ajustes para o login completo
 import { useAuth } from '../contexts/AuthContext';
